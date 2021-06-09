e7f0a35d200c8c0499a6883599a101864759e26b
Dan Powell <danpowell@DANs-MacBook-Air.local>
2021-06-09 14:00:22 -0700

    added some api functionality

--- src/views/TripPlanner.vue
+++ src/views/TripPlanner.vue
@@ -8,8 +8,24 @@
 </h2>
 </div> <!--End main header welcome container-->
 
+<main class="primary-wrapper__main">
+<div class="primary-wrapper__main__card-data-holder">
+<p>Seattle,WA temperature(C): {{temp1}}</p>
+<p>{{weather1}}</p>
+<p>{{temp1*(9/5)+32}}</p>
+</div>
+<div class="primary-wrapper__main__card-data-holder">
+<p>{{temp2}}</p>
+<p>{{weather2}}</p>
+<p>{{temp2*(9/5)+32}}</p>
 
-
+</div>
+<div class="primary-wrapper__main__card-data-holder">
+<p>{{temp2}}</p>
+<p>{{weather2}}</p>
+<p>{{temp2*(9/5)+32}}</p>
+</div>
+</main>
 
 
 
@@ -44,12 +60,84 @@ export default {
     Footer
   },
   data () {
-      return {
-          primaryHeader: "See the information below to plan your trip!"
+      return { 
+          primaryHeader: "See the information below to plan your trip!",
+          temp1: undefined,
+          weather1: undefined,
+          temp2: undefined,
+          weather2: undefined,
+          temp3: undefined,
+          weather3: undefined
+
 
   }
-  }
-}
+  },
+  methods: {
+    getData(){
+      fetch('https://api.weatherbit.io/v2.0/current?&city=Seattle,WA&key=d7b112b090fa4c8baac9453adf0ac2d2&include=minutely')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then((json) => {
+        console.log(json)
+        this.temp1 = json.data[0].temp
+        this.weather1 = json.data[0].weather.description
+      })
+    },
+     getData2(){
+      fetch('https://api.weatherbit.io/v2.0/current?&city=Issaquah,WA&key=d7b112b090fa4c8baac9453adf0ac2d2&include=minutely')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then((json) => {
+        console.log(json)
+        this.temp2 = json.data[0].temp
+        this.weather2 = json.data[0].weather.description
+      })
+    }
+    },
+     getData3(){
+      fetch('https://api.weatherbit.io/v2.0/current?&city=Bellingham,WA&key=d7b112b090fa4c8baac9453adf0ac2d2&include=minutely')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then((json) => {
+        console.log(json)
+        this.temp3 = json.data[0].temp
+        this.weather3 = json.data[0].weather.description
+      })
+    },
+
+    
+    mounted: function() {
+      this.getData()
+      this.getData2()
+
+    }
+    }
+    
+  
 
 
 </script>
+<style scoped lang="scss">
+.primary-wrapper {
+  &__main {
+      grid-column:3/11;
+      grid-row:3/10;
+      display:flex;
+      flex-direction:row;
+    &__card-data-holder {
+    margin-right: 4rem;
+  }
+
+  }
+  
+}
+
+
+
+</style>
